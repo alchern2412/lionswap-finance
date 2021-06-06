aceea9e1281f6ecb6f3bf5f255ce68f45c464036
aliaksei.charniauski <aliaksei.charniauski@yellow.systems>
2021-06-06 23:42:06 +0300

    Updates

---
+++ static/js/17.39f8b607.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonppancake-frontend"]=this["webpackJsonppancake-frontend"]||[]).push([[17],{1135:function(n,a,e){"use strict";e.r(a);var t=e(959);a.default=t.Bar}}]);
+//# sourceMappingURL=17.39f8b607.chunk.js.map
---
+++ static/js/20.fd8a5e79.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonppancake-frontend"]=this["webpackJsonppancake-frontend"]||[]).push([[20],{982:function(n,p){}}]);
+//# sourceMappingURL=20.fd8a5e79.chunk.js.map
